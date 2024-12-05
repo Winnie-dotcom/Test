e5a8225d77a98455fe6f31952927a4de2349c3c7
Winnie-dotcom <hieuzngn@gmail.com>
2024-12-05 22:48:37 +0700

    create

---
+++ Create.php
@@ -0,0 +1,8 @@
+<php
+echo "# Test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Winnie-dotcom/Test.git
+git push -u origin main
